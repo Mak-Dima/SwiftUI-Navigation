ad4946e28602cddad98ec494e964d708316b1e60
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2025-12-12 18:18:10 -0600

    Desciptions for TabView and Tab added as a comments.

--- SwiftUI-Navigation/Views/ContentView.swift
+++ SwiftUI-Navigation/Views/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        //  TabView.
+        //  A view that switches between multiple child views using interactive user interface elements.
+        //  To create a user interface with tabs, place Tabs in a TabView.
+        //
+        //  Supports different view stiles and customization.
+        //
+        //  To programmatically select different tabs, use the init(selection:content:) initializer.
+        //  You can assign a selection value to each tab using a Tab initializer that takes a value.
         TabView {
-            Tab("Split View", systemImage: "") {SplitView()}
+            
+            //  Tab.
+            //  The content for a tab and the tab’s associated tab item in a tab view.
+            //  Can be created with label, system symbol and image.
+            Tab("Split View", systemImage: "") {
+                SplitView()
+            }
             Tab("Stack", systemImage: "") {}
             Tab("Path", systemImage: "") {}
         }
